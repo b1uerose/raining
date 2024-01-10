6c18d9e445319b446b77e2beed04438cf994ec03
xiaojl <xiaojinlong@me.com>
2024-01-10 14:50:53 +0800

    1111

--- raining-controller/src/test/java/com/xiao/raining/test/stringtest/Test1.java
+++ raining-controller/src/test/java/com/xiao/raining/test/stringtest/Test1.java
@@ -1,5 +1,8 @@
 package com.xiao.raining.test.stringtest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.Test;
 
 /**
@@ -14,10 +17,19 @@ public class Test1 {
         System.out.println("hello");
 
         System.out.println("1234567890".substring(1));
-        
+
+        System.out.println("hello\n".trim());
+
+
+        String companyOrgType = "全面所有制";
+        String replacedCompanyOrgType = companyOrgType.replaceAll("\\(", "（").replaceAll("\\)", "）");
+        System.out.println(replacedCompanyOrgType);
         
         String cellName = "AA11";
         System.out.println(cellName.replaceAll("\\d+", ""));
+
+        String vfullname = "[zpl测试].[重庆中烟单位定额测试].四类烟单价";
+        System.out.println(vfullname.replaceAll("\\[", "").replaceAll("]", "").replaceAll("\\.", "/"));
 
 
         String abs = Base62.decode("xoxuxTconditionxTxhxTxkxTxdxTfieldNamexTxhxTpkxscyclexTxdxTvaluexTxhxT0001YF10000000000GZDxTxwxdxuxTconditionxTxhxTxkxTxdxTfieldNamexTxhxTpkxsdeptbgtfillxTxdxTvaluexTxhxT1001YF10000000013NDBxTxwxqx");
@@ -35,5 +47,33 @@ public class Test1 {
         System.out.println(url2.substring(url2.indexOf("ui")));
 
     }
+    
+    @Test
+    public void base62Test() throws Exception {
+        String abs = Base62.decode("BASE62_xoxuxTconditionxTxhxTxkxTxdxTfieldNamexTxhxTaxfpkxsdeptbgtfillxTxdxTvaluexTxhxT1001YF10000000017USJxTxwxqx");
+        System.out.println(abs);
+    }
+    
+    @Test
+    public void funcTest() throws Exception {
+        List<String> sList = new ArrayList<>();
+        sList.add("('11111','2222')");
+        sList.add("('11111','2222')");
+        sList.add("('11111','2222')");
+        sList.add("('11111','2222')");
+        sList.add("('11111','2222')");
+        sList.add("('11111','2222')");
+        System.out.println(String.join(",", sList));
+    }
+    
+    @Test
+    public void stringJoinTest() throws Exception {
+        String str1 = "YSM";
+        String str2 = "Y";
+
+        System.out.println(String.join("", str1, str2));
+    }
+    
+    
     
 }
---
+++ raining-controller/src/test/java/com/xiao/raining/text/color/ColorConvert.java
@@ -0,0 +1,6 @@
+package com.xiao.raining.text.color;/**
+ * 
+ * Author: xiaojl
+ * Date:2023-10-26 23:50 
+*/public class ColorConvert {
+}
